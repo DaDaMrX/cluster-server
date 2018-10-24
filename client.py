4c56e8305d89b482a4a51730f6adcc0234af26d3
dada <bjxuheng@cn.ibm.com>
2018-10-24 16:18:28 +0800

    tf-finish

--- client.py
+++ client.py
@@ -1,11 +1,23 @@
 import urllib.parse, urllib.request
+import json
 
-url = 'http://localhost:8080/cluster'
+from database import Database
+
+url = 'http://localhost:8080'
 
 data = {
-    'username': 'DaDa',
+    'bot_id': '0',
+    'convs': [],
+    'stop_words': [],
+    'max_df': 0.1,
+    'min_df': 20,
+    'n_clusters': 50,
+    'method': 'kmeans'
 }
-data = urllib.parse.urlencode(data).encode()
 
-result = urllib.request.urlopen(url, data).read().decode()
+data = json.dumps(data).encode('utf8')
+headers = {'content-type': 'application/json'}
+req = urllib.request.Request(url, data=data, headers=headers)
+
+result = urllib.request.urlopen(req).read().decode()
 print(result)
--- server.py
+++ server.py
@@ -1,10 +1,35 @@
-from bottle import post, request, run
+import bottle
 import json
+import threading
 
-@post('/cluster')
-def hello():
-    username = request.forms.get('username')
-    data = {'msg': username}
-    return json.dumps(data)
+from cluster import ClusterThread
+from database import Database
 
-run(host='localhost', port=8080, debug=True)
+
+class Server:
+    
+    def __init__(self, ip='127.0.0.1', port=8080, debug=False):
+        self.ip = ip
+        self.port = port
+        self.debug = debug,
+
+    @bottle.post('/')
+    def cluster():
+        data = bottle.request.json
+        bot_id = data['bot_id']
+        convs = data['convs']
+        stop_words = data['stop_words']
+        max_df = data['max_df']
+        min_df = data['min_df']
+        n_clusters = data['n_clusters']
+        method = data['method']
+        cluster_thread = ClusterThread(
+            bot_id, convs, stop_words, max_df, min_df, n_clusters, method)
+        cluster_thread.start()
+        return 'successfull'
+        
+    def start(self):
+        bottle.run(ip=self.ip, port=self.port, debug=self.debug)
+        
+        
+Server().start()
